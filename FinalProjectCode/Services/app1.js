164cc33307501e3e2ee45124b5601b4f17d8287d
Dren <dh49136@ubt-uni.net>
2020-12-18 00:08:24 +0100

    Update app1.js

--- FinalProjectCode/Services/app1.js
+++ FinalProjectCode/Services/app1.js
@@ -9,7 +9,7 @@ const section = document.querySelector('section');
 const tl  = new TimelineMax();
 
 
-tl.fromTo(creative,1.3, {x:"+30%",opacity:0}, {x:"0%",opacity:1, ease:Power2.easeInOut})
+tl.fromTo(creative,1.3, {x:"-30%",opacity:0}, {x:"0%",opacity:1, ease:Power2.easeInOut})
 .fromTo(servicestxt, 1 ,{x:"+30%",opacity:0}, {x:"0%",opacity:1, ease:Power2.easeInOut},"-=1.2")
 .fromTo(whileyouhave, 1 , {opacity : 0}, {opacity : 1})
 .fromTo(butoni1, 1 , { y:"-30%",opacity : 0} , { y:"0%",opacity: 1})
