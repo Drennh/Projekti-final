f9dd4e0ba1ef60d37969652f848f85eebf7890fe
BleonaBjrm <bb47890@ubt-uni.net>
2021-02-05 00:09:01 +0100

    Responsiviteti per faqen ServicesBook

--- BD-Dent Bleona Bajrami, Dren Hyseni/Services book/servicesbook.php
+++ BD-Dent Bleona Bajrami, Dren Hyseni/Services book/servicesbook.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="servicesbook.css">
+    <link rel="stylesheet" href="servicesbook.css?a">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js" 
@@ -11,29 +11,29 @@
 </head>
 <body>
     <header>
-      <a href="../Home/home.html"> <img id="logo" src="../photos/LOGO3.png" alt=""></a> 
+      <a href="../Home/home.php"> <img id="logo" src="../photos/LOGO3.png" alt=""></a> 
     </header>
     <main>
         <div class="leftside">
         </div>
         <div class="rightside">
             <h1 class="help">How can we help?</h1>
-            <a href="../Book/bookNow.html" class="a"><div class="indiv" id="div1">
+            <a href="../Book/bookNow.php" class="a"><div class="indiv" id="div1">
             <h1>Checkup</h1>
             <p class="description" >Cleaning. x-rays end exam in 90 min</p>
             </div></a>
             <!--Different Div-->
-            <a href="../Book/bookNow.html" class="a"><div class="indiv" id="div2">
+            <a href="../Book/bookNow.php" class="a"><div class="indiv" id="div2">
                 <h1>Breezy Braces</h1>
                 <p class="description" >Care-free, can't see straightening</p>
                 </div></a>
                 <!--Different Div-->
-                <a href="../Book/bookNow.html" class="a"><div class="indiv" id="div3">
+                <a href="../Book/bookNow.php" class="a"><div class="indiv" id="div3">
                     <h1>Cosmetic</h1>
                     <p class="description" >Veneers,Whitening,and more </p>
                     </div></a>
                     <!--Different Div-->
-                    <a href="../Book/bookNow.html" class="a"><div class="indiv" id="div4">
+                    <a href="../Book/bookNow.php" class="a"><div class="indiv" id="div4">
                         <h1>Procedures </h1>
                         <p class="description" >Fillings, implants, crowns and more</p>
                         </div></a>
