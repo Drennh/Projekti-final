5051c81e8c08fe785b5088250c215c32bca664a3
BleonaBjrm <bb47890@ubt-uni.net>
2021-02-24 15:29:52 +0100

    Modifikime ne Services

--- BD-Dent Bleona Bajrami, Dren Hyseni/Services/services.php
+++ BD-Dent Bleona Bajrami, Dren Hyseni/Services/services.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,7 +22,18 @@
             <li><a class="NavLinks" href="../Home/home.php">Home</a></li>
             <li><a class="NavLinks" class="services" href="#">Services</a></li>
             <li><a class="NavLinks" href="../contact/contactUs.php">Contact</a></li>
-            <li><a class="NavLinks" href="../Login/login.php">Login</a></li>
+            <?php
+
+if(!isset($_SESSION['role'])){
+
+  ?> <li><a class="NavLinks" href="../Login/login.php">Login</a></li> <?php
+}
+else if(isset($_SESSION['role'])){
+  
+ ?> <li><a class="NavLinks" href="../logOut/logOut.php">Log Out</a></li> <?php
+         
+}
+?>
            <a href="../Services book/servicesbook.php"> <button class="booknowbutoni">BOOK NOW</button></a>
         </ul>
         <div class="burger">
@@ -37,29 +52,29 @@
         <!--Kodi per responsive-->
         <div class="responsivetxt">
             <h1 class="servicestxt1">SERVICES</h1>
-            <p class="creative1">Lorem ipsum dolor sit amet sit <br> ipsum dolor sit amet dolor consectetur.</p>
+            <p class="creative1">We’re passionate about helping you <br> experience a beautiful and healthy smile.</p>
         </div>
             <div class="boxes2">
                 <div class="katrori1">
                     <img id="icons" src="../icons/check.png" alt="">
-                    <h2>Checkup</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident.</p>
+                    <h2>Whitening</h2>
+                    <p>Our special dentist would make you smile brighter than the sun</p>
 
                 </div>
                 <div class="katrori1">
                    <img id="icons" src="../icons/dentist.png" alt="">
-                   <h2>Checkup</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident.</p>
+                   <h2>Veneers</h2>
+                    <p>If you want to improve your smile , dental veneers are a simple option</p>
                 </div>
                 <div class="katrori1">
                     <img id="icons" src="../icons/root.png" alt="">
-                    <h2>Checkup</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident.</p>
+                    <h2>Cosmetic</h2>
+                    <p>Your smile is one of the first things people notice about you</p>
                 </div>
                 <div class="katrori1">
                     <img id="icons" src="../icons/tools.png" alt="">
-                    <h2>Checkup</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident.</p>
+                    <h2>Breezy Braces</h2>
+                    <p>It's never too late to get the smile you want</p>
                 </div>
             </div>
          <!--Kodi per responsive Mbarimi-->
@@ -69,41 +84,41 @@
          <!--FILLIMI I KODIT PER DIV TEKSTIN NE TE MAJTE-->
         <div class="left">
             <h1 class="servicestxt">SERVICES</h1>
-            <p class="creative">Lorem ipsum dolor sit amet sit <br> ipsum dolor sit amet dolor consectetur.</p>
+            <p class="creative">We’re passionate about helping you <br> experience a beautiful and healthy smile.</p>
         </div>
          <!--MBARIMI I KODIT PER DIV TEKSTIN NE TE MAJTE-->
 
           <!--FILLIMI I KODIT PER DIV KATRORET E TEJDUKSHEM ME FOTO DHE TEKST BRENDA-->
         <div class="div2"id="col">
            <img id="icons" src="../icons/check.png" alt=""> <br>
-            <h3 id="txt">Checkup</h3>
-            <p id="txt" class="ptext" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident.</p>
+            <h3 id="txt">Whitening</h3>
+            <p id="txt" class="ptext" >Our special dentist would make you smile brighter than the sun, two shades or more</p>
         </div>
         <div class="box3"id="col">
             <img id="icons" src="../icons/dentist.png" alt="">  <br>
-            <h3 id="txt">Breezy Braces</h3>
-            <p id="txt" class="ptext" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident.</p>
+            <h3 id="txt">Veneers</h3>
+            <p id="txt" class="ptext" >If you want to improve your smile , dental veneers are a simple option</p>
         </div>
         <div class="div1"id="col">
             <img id="icons" src="../icons/root.png" alt=""><br>
             <h3 id="txt">Cosmetic</h3>
-            <p id="txt" class="ptext" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident.</p>
+            <p id="txt" class="ptext" >Your smile is one of the first things people notice about you, let's make it shine.</p>
      </div>
         <div class="box1" id="col">
             <img id="icons" src="../icons/tools.png" alt=""><br>
-            <h3 id="txt">Procedures</h3>
-            <p id="txt" class="ptext" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident.</p>
+            <h3 id="txt">Check Up</h3>
+            <p id="txt" class="ptext" > We offer treatment solutions for every member of the family, give tour self and your loved ones a chech up</p>
         </div>
         <div class="box2"id="col">
             <img id="icons" src="../icons/tooth.png" alt="">
-            <h3 id="txt">Breezy Braces</h3>
-            <p id="txt" class="ptext" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident.</p>
+            <h3 id="txt">Implants</h3>
+            <p id="txt" class="ptext" >Perhaps decay moved too deep into the roots, or maybe you had a run in with a rogue softball!</p>
         </div>
       
        <div class="div"id="col">
            <img id="icons" src="../icons/braces.png" alt="">
-        <h3 id="txt">Procedures</h3>
-            <p id="txt" class="ptext" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident.</p>
+        <h3 id="txt">Breezy Braces</h3>
+            <p id="txt" class="ptext" >It's never too late to get the smile you want, especially now that Breezy Braces are here</p>
        </div>
      
      <!--MBARIMI I KODIT PER DIV KATRORET E TEJDUKSHEM ME FOTO DHE TEKST BRENDA-->
